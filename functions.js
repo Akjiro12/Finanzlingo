1dc2d56173c8bc2a71bf1e23e9389c24cb11e91a
Akjiro12 <164920223+Akjiro12@users.noreply.github.com>
2024-03-26 00:42:23 +0100

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,14 @@
+
+function chisiamo(){
+
+    window.location.href="pagina_presentazione.html";
+}
+function lezioni (){
+   
+    window.location.href="pagina_lezioni.html";
+
+}
+
+function indietro(){
+    window.location.href="index.html"; 
+}
