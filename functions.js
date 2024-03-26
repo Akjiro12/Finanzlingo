bf9fcdcfa514ab7d1d750d9b4076d8464a60fb61
Akjiro12 <164920223+Akjiro12@users.noreply.github.com>
2024-03-27 00:10:07 +0100

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,46 @@
+
+function chisiamo(){
+
+    window.location.href="pagina_presentazione.html";
+}
+function lezioni (){
+   
+    window.location.href="pagina_lezioni.html";
+
+}
+function contattaci (){
+    window.location.href="pagina_contattaci.html";
+}
+
+function indietro(){
+    window.location.href="home.html"; 
+}
+function login(){
+  window.location.href="index.html";
+}
+
+function LogIn(){
+  window.location.href="Log in.html";
+  
+}
+
+var carousel = $(".carousel"),
+    currdeg  = 0;
+
+$(".next").on("click", { d: "n" }, rotate);
+$(".prev").on("click", { d: "p" }, rotate);
+
+function rotate(e){
+  if(e.data.d=="n"){
+    currdeg = currdeg - 60;
+  }
+  if(e.data.d=="p"){
+    currdeg = currdeg + 60;
+  }
+  carousel.css({
+    "-webkit-transform": "rotateY("+currdeg+"deg)",
+    "-moz-transform": "rotateY("+currdeg+"deg)",
+    "-o-transform": "rotateY("+currdeg+"deg)",
+    "transform": "rotateY("+currdeg+"deg)"
+  });
+}
